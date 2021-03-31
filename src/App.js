3a3b3ca772ef20f649cfff8d7270785da57d002d
ChirazG <79950267+ChirazG@users.noreply.github.com>
2021-03-31 02:30:31 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+ class App extends Component {
+   constructor (props){
+     super(props)
+     this.state={
+      fullName:"Chiraz GOUISSEM",
+      bio: "Chiraz",
+      imgSrc:"/ch.jpg",
+      profession:"Process Engineer",
+      show: false,
+      time: 0
+     }
+   }
+    showProfile=()=>{
+    this.setState({show:!this.state.show,
+    time: 0}) 
+}
+componentDidMount() {
+setInterval(() => {this.setState({time: this.state.time+1})},1000)
+}
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.show? <><h1>Hello my name is {this.state.fullName}</h1> 
+        <h1>My bio: {this.state.bio}</h1>
+        <img src={this.state.imgSrc} alt="my photo"></img>
+        <h1>I'm a {this.state.profession}</h1>
+        <h1>{this.state.time} s</h1></> : null}
+        <button onClick={ () => this.showProfile()}>Click here</button>
+      </div>
+    )
+  }
 }
 
 export default App;
